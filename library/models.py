d2cdda768698d70743ec22db1745facb7e36dc92
unknown <ps4spielevladi@gmail.com>
2025-11-08 13:16:16 +0300

    Add class Author

--- library/models.py
+++ library/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Author(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=50)
@@ -8,3 +7,9 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.last_name[0]}. {self.first_name}"
+
+"""Обновите уже существующую модель Author дополнительными полями:
+Профиль: ссылка на личную страницу автора, может быть не указана
+Удалён: поле, которое позволит смотреть удалён ли этот автор из базы всех авторов.
+По умолчанию все авторы активны
+Рейтинг: позволит отсматривать рейтинг популярности авторов, от 1 до 10"""
